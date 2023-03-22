476e123a54838cd703103fc5e94b212c82daa12e
injashkin <injashkin@gmail.com>
2023-03-22 21:50:28 +0300

    Add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,87 @@
+import logo from "../assets/logo-white.svg";
+import chevron from "../assets/chevron.svg";
+import download from "../assets/download-white.svg";
+import whatsapp from "../assets/whatsapp.svg";
+import telegram from "../assets/telegram.svg";
+import visa from "../assets/visa.svg";
+import masterCard from "../assets/master-card.svg";
+
+import "./Footer.css";
+import Search from "./Search";
+import Button from "./Button";
+
 export default function Footer() {
   return (
-    <footer className="page-footer teal lighten-1">
-      <div className="container">
-        © {new Date().getFullYear()} All rights reserved
-      </div>
-    </footer>
+    <div className="footer-wrap">
+      <footer className="footer container">
+        <div className="footer__col1">
+          <img src={logo}></img>
+          <div>
+            Компания «Султан» — снабжаем розничные магазины товарами "под ключ"
+            в Кокчетаве и Акмолинской области
+          </div>
+          <div>Подпишись на скидки и акции</div>
+          <Search
+            text="Введите ваш E-mail"
+            icon={chevron}
+            parentClass="footer"
+          ></Search>
+        </div>
+        <div className="footer__col2">
+          <h3>Меню сайта:</h3>
+          <nav>
+            <li>О компании</li>
+            <li>Доставка и оплата</li>
+            <li>Возврат</li>
+            <li>Контакты</li>
+          </nav>
+        </div>
+        <div className="footer__col3">
+          <h3>Категории:</h3>
+          <nav>
+            <li>Бытовая химия</li>
+            <li>Косметика и гигиена</li>
+            <li>Товары для дома</li>
+            <li>Товары для детей и мам</li>
+            <li>Посуда</li>
+          </nav>
+        </div>
+
+        <div className="footer__col4">
+          <h3>Скачать прайс-лист:</h3>
+          <Button
+            text="Прайс-лист"
+            icon={download}
+            parentClass="footer"
+            name={""}
+          ></Button>
+          <div>Связь в мессенджерах:</div>
+          <div>
+            <a href="">
+              <img src={whatsapp} />
+            </a>
+            <a href="">
+              <img src={telegram} />
+            </a>
+          </div>
+        </div>
+
+        <div className="footer__col5">
+          <h3>Контакты:</h3>
+          <div>+7 (777) 490-00-91</div>
+          <div>время работы: 9:00-20:00</div>
+          <div>Заказать звонок</div>
+          <div>
+            opt.sultan@mail.ru
+            <br />
+            <span>На связи в любое время</span>
+          </div>
+          <div>
+            <img src={visa}></img>
+            <img src={masterCard}></img>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
