9226fed3aadfc7dc9eeb92ec85bfee948ebdcebe
injashkin <injashkin@gmail.com>
2023-04-03 15:48:50 +0300

    Production

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import path from 'path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/e-shop/',
+  base: '/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@ui': path.resolve('src', 'ui'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve('index.html'),
-        nested: path.resolve('404.html'),
-      },
-    },
-  },
 })
